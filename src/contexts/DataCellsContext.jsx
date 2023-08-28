b55c9c1c922e62b6e4100cc54d431edc61ea66a3
Vane Arapa <vanessaah.lu@gmail.com>
2023-08-28 09:43:41 -0500

    add changes in DataCellsContext

--- src/contexts/DataCellsContext.jsx
+++ src/contexts/DataCellsContext.jsx
@@ -2,7 +2,6 @@ import { createContext, useEffect, useState } from "react";
 // import mockApi from "../helpers/mockApi.js";
 
 import {
-    peopleAll,
     filterDataByName,
   }  from "../helpers/data.js";
 
@@ -10,29 +9,38 @@ export const DataCellsContext = createContext({});
 
 export const DataCellsProvider = ({ children }) => {
 
-    const [loadingCell, setLoadingCell] = useState(false);
+    const [loadingCell, setLoadingCell] = useState(true);
     const [noticeCell, setNoticeCell] = useState(false);
 
     const [user, setUser] = useState("");
-    const [page, setPage] = useState(9);
+    const [page, setPage] = useState(3);
     const [dataCells, setDataCells] = useState([]);
     const [dataVehiculesCells, setDataVehiculesCells] = useState([]);
     const [personCells, setPersonCells] = useState([]);
 
+
     useEffect(()=> {
 
         let promiseLoop = [];
+        setNoticeCell(false)
         for (let i = 0; i < page; i++){
-          setLoadingCell(true);
           promiseLoop[i] = fetch(`https://swapi.dev/api/people/?page=${i+1}`)
-          .then(res => res.json());
+          .then(res => res.json())
         }
      
-        Promise.all(promiseLoop).then(values => {
+        Promise.all(promiseLoop)
+        .then(values => {
            setLoadingCell(false)
            const dataAll= values.flatMap(value => [...value.results]);
-           setPersonCells(dataAll);
-        });
+           setPersonCells(dataAll)
+        })
+        .catch(error => 
+          {
+            setLoadingCell(false)
+            setNoticeCell(true)
+          }
+          )
+        ;
 
          setDataCells(filterDataByName(personCells, user ));
          setDataVehiculesCells( filterDataByName(personCells, user, "yes"));
